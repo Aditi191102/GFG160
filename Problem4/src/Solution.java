09d7afa3c0a6832595eb207908b1555e3d626bb5
Aditi191102 <105860777+Aditi191102@users.noreply.github.com>
2024-11-18 23:57:58 +0530

    Optimized solution added

--- Problem4/src/Solution.java
+++ Problem4/src/Solution.java
@@ -1,20 +1,43 @@
 public class Solution {
     void rotateArr(int arr[], int d) {
         // add your code here
-        int n=arr.length;
-        for(int i=0;i<d;i++)
-        {
-            int temp=arr[0];
-            for(int j=1;j<n;j++)
-            {
-                arr[j-1]=arr[j];
-                if(j==n-1)
-                    arr[j]=temp;
+        int n = arr.length;
+//        for(int i=0;i<d;i++)
+//        {
+//            int temp=arr[0];
+//            for(int j=1;j<n;j++)
+//            {
+//                arr[j-1]=arr[j];
+//                if(j==n-1)
+//                    arr[j]=temp;
+//            }
+//        }
+
+        d=d%n;
+        if (d <= n) {
+            for (int i = 0, j = d - 1; i < d / 2; i++) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+
+            for (int i = d, j = n - 1; i < j; i++) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+            for (int i = 0, j = n - 1; i < n / 2; i++) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j--;
             }
         }
-        for (int i=0;i<n;i++)
-        {
-            System.out.print(arr[i]+" ");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
         }
     }
 }
